6a79b5606e3d8f30fc21cae9e3041f58b9bb2172
Maycon <mayconrodrigo123@hotmail.com>
2022-01-25 14:06:33 -0300

    token

--- src/app/autenticacao/token.service.ts
+++ src/app/autenticacao/token.service.ts
@@ -6,20 +6,30 @@ const KEY = 'token'
   providedIn: 'root'
 })
 export class TokenService {
+
+  temToken: boolean
+
   retornaToken() {
+    //buscando a KEY, caso venha sem valor, deve retornar em branco
     return localStorage.getItem(KEY) ?? '';
   }
 
   salvaToken(token: string) {
+    //setar valor para variavel token - setItem(key: string, value: string)
     localStorage.setItem(KEY, token)
   }
 
   excluiToken() {
+    //limpar token
     localStorage.removeItem(KEY)
   }
 
   possuiToken() {
-    return !! this.retornaToken();
+    //retorna boleano
+    //return !! this.retornaToken();
+    if (this.retornaToken().length > 1) {
+      this.temToken = true;
+    }
   }
 
 }
